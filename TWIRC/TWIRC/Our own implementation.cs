2d3e17902e01fbdb0dcb27cf2350687b1e89ff2a
Guido "Harb" <harbingerofme@gmail.com>
2014-11-30 23:23:37 +0100

    More stuff
    WOOOO

--- TWIRC/TWIRC/Our own implementation.cs
+++ TWIRC/TWIRC/Our own implementation.cs
@@ -11,41 +11,53 @@ namespace TWIRC
         public static IrcClient irc = new IrcClient();
         public static IrcClient irc2 = new IrcClient();//backup connection (to check if our messages arive)
 
+        public string bot_name = "HarbBot";//gonna hardcode this in for now, but will be loaded from a file soonish
+        public string[] channels = { "#rngplayspokemon","#harbbot","harbingerofme"}
+        public string oauth = "oauth:474uj7qa2pqjcjevugz8sph6ncq4rd0";//might be invalid
+
+
         public static void Main(string[] args)
         {
+
+            //loadInformation();
+
             Thread.CurrentThread.Name = "Main";
             irc.Encoding = System.Text.Encoding.UTF8;//twitch's encoding
             irc.SendDelay = 1500;
             irc.ActiveChannelSyncing = true;
-            
+
+
+
             //write these Methods
             irc.OnConnected += ircConnected;
             irc.OnJoin += ircJoined;
             irc.OnConnectionError += ircConError;
             irc.OnError += ircError;
+            irc2 = irc;
             irc.OnQueryMessage += ircQuery;
             irc.OnRawMessage += ircRaw;
+            irc2.OnQueryMessage += irc2Query;
+            irc2.onRawMessage += irc2Raw;
 
             try//connection test
             {
                 
                 irc.Connect("irc.twitch.tv", 6667);
+                irc2.Connect("irc.twitch.tv",6667);
             }
             catch (ConnectionException e)
             {
                 System.Console.WriteLine("Connection error: " + e.Message);
             }
-            try
-            {
-                irc.RfcJoin("#harbbot");
-                irc.Listen();
-                irc.Disconnect();
-            }
-            catch
-            {
-
-            }
             
         }
+
+       public void ircConnected(object sender, EventArgs e)
+       {
+           IrcClient a = (IrcClient) sender;
+           a.Login(bot_name,"HARBBOT",0,bot_name,oauth);
+           a.RfcJoin(channels);
+           a.Listen();
+       }
     }
 }
